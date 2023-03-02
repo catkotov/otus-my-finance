8114706fc27bc385c57fd6943781d1d944be732b
Котов Александр <aleksandr.a.kotov@gazprombank.ru>
2023-03-02 23:23:58 +0300

    Comment was added

--- m1l1-greeting/src/main/kotlin/org/cat/eye/otus/finance/init/App.kt
+++ m1l1-greeting/src/main/kotlin/org/cat/eye/otus/finance/init/App.kt
@@ -1,5 +1,8 @@
 package org.cat.eye.otus.finance.init
 
+/**
+ * Enter your name as a first element of arguments.
+ */
 fun main(args : Array<String>) {
 
     val name = if (args.isNotEmpty()) args[0] else "student"
